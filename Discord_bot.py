eaf6534d68b6dc14bcf2ba3d5770688c9e161292
E.Lochan <63242350+Lochan9@users.noreply.github.com>
2021-05-30 17:09:24 +0530

    Add files via upload

---
+++ Discord_bot.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+import os
+
+client = commands.Bot(command_prefix = ">")
+
+@client.event
+async def on_ready():
+  print(" bot is ready")
+
+
+@client.command()
+async def hello(ctx):
+  await ctx.send("YO MF")
+
+@client.command(name='spam', help='Spams the input message for x number of times')
+async def spam(ctx, amount:int, *, message):
+    for i in range(amount): # Do the next thing amount times
+        await ctx.send(message) # Sends message where command was called
+
+
+@client.command()
+async def clear(ctx,amount=5):
+    await ctx.channel.purge(limit=amount)
+
+@client.command()
+async def kick(ctx,member: discord.Member,*, reason=None):
+    await member.kick(reason=reason)
+
+
+client.run("ODQ3NDM1NzM5NDczOTAzNjM2.YK-CCA.umOduu6umOUxo5hdba_gNTVLLdk")
